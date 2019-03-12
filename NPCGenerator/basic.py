abbc6f6ea759d4580d83323b5373e8623791cc21
Ali-Gregory <alaethea.gamblin@gmail.com>
2019-03-12 21:33:06 +0000

    Create basic.py

---
+++ NPCGenerator/basic.py
@@ -0,0 +1,31 @@
+import random
+
+race_list = ("DRAGONBORN","DWARF","ELF","GNOME","HALF-ELF","HALF-ORC","HALFING",
+        "HUMAN","TIEFLING")
+dra_sub = ("Black","Blue","Brass","Bronze","Copper","Gold","Green","Red",
+           "Silver","White")
+dwa_sub = ("Hill","Mountain")
+elf_sub = ("Drow","High","Wood")
+
+race_choice = race_list[random.randint(0,8)]
+
+print(race_choice)
+
+
+def sub_race(race):
+    return race_full
+
+'''class NPC:
+    name
+    race
+    subrace
+    npc_class
+    level
+    alignment
+    
+    strength
+    dex
+    const
+    intell
+    wisd
+    charis'''
